af6c282f336ccdb4a2c76fd6ecdd56f438fd8cea
Eden Wang <yociduo@vip.qq.com>
2018-03-14 20:01:01 +0800

    update room page

--- client/pages/room/index.js
+++ client/pages/room/index.js
@@ -3,20 +3,34 @@ const cards = require('../../utils/cards.js');
 
 Page({
   data: {
+    players: []
   },
   onLoad: function (options) {
-    const { needScore, isHost } = app.globalData;
+    const { needScore = false, isHost = false } = app.globalData;
     const { id, name: _name, story: _story } = options;
     const name = _name ? decodeURIComponent(_name) : 'Room';
     const story = _story ? decodeURIComponent(_story) : 'Story';
 
     this.setData({ isHost, needScore, id, name, story, cards });
+
+    // mock players
+    wx.getUserInfo({
+      success: ({userInfo}) => {
+        console.log(userInfo)
+        this.setData({ players: [userInfo] });
+      }
+    });
   },
 
   onShareAppMessage: function () {
-
-    
     const { name: title, story: desc } = this.data;
-    return { title, desc }
+    return { title, desc };
+  },
+
+  onCardClick: function (e) {
+    if (true) {
+      const { value: selectedValue } = e.target.dataset;
+      this.setData({ selectedValue });
+    }
   }
-})
+});
--- client/utils/cards.js
+++ client/utils/cards.js
@@ -1,81 +1,68 @@
 const cards = [
   {
     key: '0',
-    center: '0',
     corner: '0',
     value: 0
   },
   {
     key: '1/2',
-    center: '1/2',
     corner: '1/2',
     value: 0.5
   },
   {
     key: '1',
-    center: '1',
     corner: '1',
     value: 1
   },
   {
     key: '2',
-    center: '2',
     corner: '2',
     value: 2
   },
   {
     key: '3',
-    center: '3',
     corner: '3',
     value: 3
   },
   {
     key: '5',
-    center: '5',
     corner: '5',
     value: 5
   },
   {
     key: '8',
-    center: '8',
     corner: '8',
     value: 8
   },
   {
     key: '13',
-    center: '13',
     corner: '13',
     value: 13
   },
   {
     key: '20',
-    center: '20',
     corner: '20',
     value: 20
   },
   {
     key: '40',
-    center: '40',
     corner: '40',
     value: 40
   },
   {
     key: '100',
-    center: '100',
     corner: '100',
     value: 100
   },
   {
     key: '?',
-    center: '?',
     corner: '?',
-    value: 0
+    value: -1
   },
   {
-    key: 'coffee',
-    center: 'C',
+    key: 'C',
     corner: 'coffee',
-    value: 0
+    value: -2
   }
 ];
 
