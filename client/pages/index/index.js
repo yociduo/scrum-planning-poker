eb4205b8d0039890bc943e22071b52eb8164ea0b
Eden Wang <yociduo@vip.qq.com>
2018-03-15 21:17:45 +0800

    update index page

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -2,20 +2,25 @@ const app = getApp();
 
 Page({
   data: {
-
+    room: 'Room',
   },
   onLoad: function () {
 
   },
+  onRoomChange: function (e) {
+    const { value: room } = e.detail;
+    this.setData({ room });
+  },
   formSubmit: function (e) {
-    const { room, story, needScore } = e.detail.value;
-    const id = Math.ceil(Math.random() * 10000);
+    const { room, stories, needScore } = e.detail.value;
+    if (room) {
+      const id = Math.ceil(Math.random() * 10000).toString();
 
-    app.globalData.needScore = needScore;
-    app.globalData.isHost = true;
-
-    wx.navigateTo({
-      url: `../room/index?id=${id}&name=${room || 'Room'}&story=${story || 'Story'}`,
-    });
+      app.globalData.isHost = true;
+      
+      wx.navigateTo({
+        url: `../room/index?id=${id}&name=${room}&stories=${stories}&needScore=${needScore}`,
+      });
+    }   
   }
 })
