16298eb063a24836bae7cabfa65c04cd924ad9da
Eden Wang <yociduo@vip.qq.com>
2018-03-15 21:37:19 +0800

    set hosted rooms

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -14,10 +14,16 @@ Page({
   formSubmit: function (e) {
     const { room, stories, needScore } = e.detail.value;
     if (room) {
-      const id = Math.ceil(Math.random() * 10000).toString();
-
-      app.globalData.isHost = true;
-      
+      const id = Math.ceil(Math.random() * 10000000).toString();
+      const { keys } = wx.getStorageInfoSync();
+      let hosted;
+      if (keys.includes('hosted')) {
+        hosted = wx.getStorageSync('hosted');
+        hosted.push(id);
+      } else {
+        hosted = [id];
+      }
+      wx.setStorageSync('hosted', hosted);
       wx.navigateTo({
         url: `../room/index?id=${id}&name=${room}&stories=${stories}&needScore=${needScore}`,
       });
