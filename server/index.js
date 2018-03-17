47f47c5996cea2e08ccc0b7cd52271cb99844319
Eden Wang <yociduo@vip.qq.com>
2018-03-17 08:46:42 +0800

    update mock host

--- server/index.js
+++ server/index.js
@@ -2,21 +2,44 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+const port = process.env.PORT || 9001;
+const debug = !(process.env.PROD || false);
+const log = debug ? (...args) => console.log(...args) : () => {};
 
-io.set('origins', '*:*');
-
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.broadcast.emit('hi');
-  socket.on('chat message', function (msg) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+if (debug) {
+  app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
   });
+
+  app.get('/host', (req, res) => {
+    res.sendFile(__dirname + '/host.html');
+  });
+}
+
+// rooms dictionary
+const rooms = {};
+
+io.on('connection', (socket) => {
+  log('a user connected');
+
+  socket.on('init room', ({ stories, ...room }) => {
+    log('[init room]', { stories, ...room });
+    if (!rooms.hasOwnProperty(room.id)) {
+      room.stories = decodeURIComponent(stories).split('\n').filter(i => i);
+      room.players = [],
+      room.scores = [],
+      room.currentStoryIndex = -1;
+      rooms[room.id] = room;
+    }
+  });
+
+  if (debug) {
+    socket.on('chat message', function (msg) {
+      io.emit('chat message', msg);
+    });
+  }
 });
 
-http.listen(9001, function () {
-  console.log('listening on *:9001');
+http.listen(port, () => {
+  log(`listening on *:${port}`);
 });
