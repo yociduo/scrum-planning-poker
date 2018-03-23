83a78e0f7fcb271697518b65d99cee0ed75ee18c
Eden D Wang <eden.d.wang@pwc.com>
2018-03-23 08:20:26 +0800

    update action for debug in prod mode

--- client/app.js
+++ client/app.js
@@ -12,6 +12,40 @@ App({
     this.globalData.socket = socket;
   },
   onShow() {
+    wx.getClipboardData({
+      success: ({ data }) => {
+        if (data === 'debug') {
+          wx.showModal({
+            title: 'Enable Debugging',
+            content: 'Enable Debugging',
+            success: () => {
+              wx.setClipboardData({ data: '' });
+              wx.setEnableDebug({ enableDebug: true });
+            }
+          });
+        } else if (data === 'prod') {
+          wx.showModal({
+            title: 'Disable Debugging',
+            content: 'Disable Debugging',
+            success: () => {
+              wx.setClipboardData({ data: '' });
+              wx.setEnableDebug({ enableDebug: false });
+            }
+          });
+        } else if (data === 'clear-cache') {
+          wx.showModal({
+            title: 'Clear Storage',
+            content: 'Clear Storage',
+            success: () => {
+              wx.setClipboardData({ data: '' });
+              wx.clearStorageSync();
+              wx.reLaunch({ url: 'index' });
+            }
+          })
+        }
+      }
+    })
+
     this.globalData.socket.connect();
   },
   globalData: {
