b17119bfaa25af91ecd98aa5c99ee0a8c019c95c
Eden Wang <yociduo@vip.qq.com>
2018-03-25 10:57:44 +0800

    update room detail page

--- client/pages/room-detail/index.js
+++ client/pages/room-detail/index.js
@@ -1,66 +1,26 @@
-// pages/room-detail/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+  onLoad(options) {
+    const { id } = options;
+    const room = wx.getStorageSync(id);
+    room.id = id;
+    room.scores.forEach((s, id) => s.id = id);
+    this.setData({ ...room });
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onRoomChange(e) {
+    this.setData({ name: e.detail.value });
+    const room = wx.getStorageSync(this.data.id);
+    room.name = e.detail.value;
+    wx.setStorageSync(this.data.id, room);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  kindToggle: function (e) {
+    const id = e.currentTarget.id, scores = this.data.scores;
+    for (let i = 0, len = scores.length; i < len; ++i) {
+      if (scores[i].id == id) {
+        scores[i].open = !scores[i].open
+      } else {
+        scores[i].open = false
+      }
+    }
+    this.setData({ scores });
   }
-})
+});
