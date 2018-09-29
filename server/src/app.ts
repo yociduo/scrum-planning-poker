ebce073ddc866fcb813800f7ec243232ee0fa925
yociduo <yociduo@vip.qq.com>
2018-09-29 08:39:11 +0800

    init server project

---
+++ server/src/app.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { createKoaServer, useContainer } from 'routing-controllers';
+import { Container } from 'typedi';
+import { createConnection } from 'typeorm';
+
+createConnection().then(async () => {
+  /**
+   * Setup routing-controllers to use typedi container.
+   */
+  useContainer(Container);
+
+  /**
+   * We create a new koa server instance.
+   * We could have also use useKoaServer here to attach controllers to an existing koa instance.
+   */
+  const koaApp = createKoaServer({
+    /**
+     * We can add options about how routing-controllers should configure itself.
+     * Here we specify what controllers should be registered in our express server.
+     */
+    controllers: [
+    ],
+  });
+
+  /**
+   * Start the koa app.
+   */
+  koaApp.listen(3000);
+
+  console.log('Server is up and running at port 3000');
+
+}).catch(error => console.log(error));
