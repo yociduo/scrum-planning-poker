2d010709498315f891663a075f418a3e69fdcee3
yociduo <yociduo@vip.qq.com>
2018-10-11 13:51:41 +0800

    update story list page

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,3 +1,4 @@
+const { apiUrl } = require('../../config');
 const app = getApp();
 
 Page({
@@ -5,11 +6,24 @@ Page({
     rooms: []
   },
   onShow() {
-    const rooms = (wx.getStorageSync('hosted') || []).reverse().map(id => {
-      const { name, count, time, total, finished } = wx.getStorageSync(id);
-      return { id, name, count, time, total, finished };
+    // const rooms = (wx.getStorageSync('hosted') || []).reverse().map(id => {
+    //   const { name, count, time, total, finished } = wx.getStorageSync(id);
+    //   return { id, name, count, time, total, finished };
+    // });
+    // this.setData({ rooms });
+
+    wx.request({
+      url: `${apiUrl}/rooms`,
+      method: 'GET',
+      header: {
+        'Authorization': `Bearer ${wx.getStorageSync('token')}`
+      },
+      success: ({ data: rooms, statusCode }) => {
+        if (statusCode === 200) {
+          this.setData({ rooms });
+        }
+      }
     });
-    this.setData({ rooms });
   },
   onCreateTap() {
     wx.navigateTo({ url: '../create-room/index' });
