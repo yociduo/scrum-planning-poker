b8c49d83c6c78a325d733cb8fc197cb298cb461e
Eden Wang <yociduo@vip.qq.com>
2018-11-14 15:24:13 +0800

    update login and check updated functions

--- client/src/mixins/login-mixin.js
+++ client/src/mixins/login-mixin.js
@@ -1,9 +1,14 @@
 import wepy from 'wepy';
 
 export default class LoginMixin extends wepy.mixin {
-  onLoad() {
+  async onLoad() {
     if (!this.$parent.globalData.token) {
-      this.$redirect('welcome');
+      const res = await wepy.getSetting();
+      if (res.authSetting['scope.userInfo']) {
+        await this.$parent.login();
+      } else {
+        this.$redirect('welcome');
+      }
     }
   }
 }
