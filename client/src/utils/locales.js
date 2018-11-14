264a53cb32daaef51a5016ad41e7c5b4249ffa5a
Eden Wang <yociduo@vip.qq.com>
2018-11-14 16:37:57 +0800

    add show modal intercept

--- client/src/utils/locales.js
+++ client/src/utils/locales.js
@@ -1,5 +1,7 @@
 export default {
   'en': {
+    'common.button.confirm': 'OK',
+    'common.button.cancel': 'Cancel',
     'welcome': {
       'title': 'Scrum Planning Poker',
       'text': 'Scrum Planning Poker makes agile project planning and estimation more accurate, simple, and fun.',
@@ -7,6 +9,8 @@ export default {
     }
   },
   'zh_CN': {
+    'common.button.confirm': '确定',
+    'common.button.cancel': '取消',
     'welcome': {
       'title': 'Scrum敏捷估算',
       'text': 'Scrum扑克让敏捷项目估算更加精确简单并充满乐趣',
