cb4daa4858bee1cc7f7e88f9b585ef4badddd5ac
Eden Wang <yociduo@vip.qq.com>
2018-11-14 11:39:49 +0800

    update language in welcome

--- client/src/utils/locales.js
+++ client/src/utils/locales.js
@@ -1,16 +1,16 @@
 export default {
   'en': {
     'welcome': {
-      'title': '戆卵子',
-      'text': 'Ready to enter the game?',
+      'title': 'Scrum Planning Poker',
+      'text': 'Scrum Planning Poker makes agile project planning and estimation more accurate, simple, and fun.',
       'button': 'Let\'s Go!'
     }
   },
   'zh_CN': {
     'welcome': {
-      'title': '戆卵子',
-      'text': '戴哥戆卵子',
-      'button': '戆卵子'
+      'title': 'Scrum敏捷估算',
+      'text': 'Scrum扑克让敏捷项目估算更加精确简单并充满乐趣',
+      'button': '开始'
     }
   }
 };
