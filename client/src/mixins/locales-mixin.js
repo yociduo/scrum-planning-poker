5b665dbc34f955e00db612c5255dea0e6a4d00ba
Eden Wang <yociduo@vip.qq.com>
2018-11-16 15:38:08 +0800

    set language key when user manunal select

--- client/src/mixins/locales-mixin.js
+++ client/src/mixins/locales-mixin.js
@@ -1,5 +1,5 @@
 import wepy from 'wepy';
-import { defaultLang } from '@/utils/utils';
+import { defaultLang, LANG_KEY } from '@/utils/utils';
 import locales from '@/utils/locales';
 import T from '@/utils/i18n';
 
@@ -17,6 +17,13 @@ export default class LocalesMixin extends wepy.mixin {
     currentPage: ''
   }
 
+  switchLanguage(lang) {
+    wepy.setStorageSync(LANG_KEY, lang);
+    this.$parent.globalData.lang = lang;
+    T.setLocale(lang);
+    this.setLocales();
+  }
+
   setLocales() {
     const _ = wepy.T._;
     const pageLocales = _(this.currentPage);
@@ -27,7 +34,7 @@ export default class LocalesMixin extends wepy.mixin {
   onLoad() {
     const pages = getCurrentPages(); // eslint-disable-line
     const currentPage = pages[(pages.length - 1)];
-    this.currentPage = currentPage ? currentPage.route.split('pages/')[1] : 'landing';
+    this.currentPage = currentPage ? currentPage.route.split('pages/')[1] : 'index';
     this.$apply();
     this.setLocales();
   }
