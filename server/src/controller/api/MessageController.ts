6f96de4e69255ae5fc894ca63c65b15d5cdac69d
Eden Wang <yociduo@vip.qq.com>
2019-04-27 12:04:41 +0800

    add signature functions

---
+++ server/src/controller/api/MessageController.ts
@@ -0,0 +1,21 @@
+import { JsonController, Get, QueryParam } from 'routing-controllers';
+import { Service } from 'typedi';
+import { md } from 'node-forge';
+import { config } from '../../config';
+
+@Service()
+@JsonController('/messages')
+export class MessageController {
+
+  @Get('/signature')
+  signature(
+    @QueryParam('signature') signature: string,
+    @QueryParam('timestamp') timestamp: string,
+    @QueryParam('nonce') nonce: string,
+  ) {
+    const sha1 = md.sha1.create();
+    sha1.update([config.jwtSecret, timestamp, nonce].sort().join(''));
+    return sha1.digest().toHex() === signature;
+  }
+
+}
