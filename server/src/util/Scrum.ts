a55cbdafb4badaa2e86a3de7ad2349c41aae328d
Eden Wang <yociduo@vip.qq.com>
2019-12-20 16:57:58 +0800

    (feat): Add scurm test case

--- server/src/util/Scrum.ts
+++ server/src/util/Scrum.ts
@@ -1,9 +1,236 @@
-import { Room } from '../entity/Room';
+import { getManager } from 'typeorm';
+import { Room, Story, User, UserRoom, Score } from '../entity';
+import { CalcMethod } from '../model';
 
 export class Scrum {
 
+  public static readonly initResults = Array(31)
+    .fill(null)
+    .map((v, i) => i)
+    .concat([0.5, 40, 55, 89, 100])
+    .sort((i, j) => i - j);
+
+  public static async getRoom(id: number): Promise<Room> {
+    return await getManager().findOneOrFail(Room, {
+      relations: [
+        'userRooms',
+        'stories',
+        'stories.scores',
+      ],
+      where: {
+        id,
+      },
+    });
+  }
+
   private timer?: NodeJS.Timer;
 
-  constructor(public room: Room) {
+  private onDestory?: (scrum?: Scrum) => void;
+
+  public room: Room;
+
+  public currentStory: Story = null;
+
+  public currentScore: number = null;
+
+  constructor(room: Room, onDestory?: (scrum?: Scrum) => void) {
+    this.room = room;
+    this.onDestory = onDestory;
   }
+
+  public async join(user: User): Promise<void> {
+    const userRoom = await this.createUserRoom(user, false);
+    await this.handleTimer(user, userRoom);
+  }
+
+  public async leave(user: User): Promise<void> {
+    const userRoom = await this.createUserRoom(user, true);
+    await this.handleTimer(user, userRoom);
+  }
+
+  public async selectCard(user: User, card: number): Promise<void> {
+    if (!this.currentStory) return;
+    const score = this.currentStory.scores.find(s => s.userId === user.id);
+    score.card = card;
+    score.timer = this.currentStory.timer;
+    await getManager().save(Score, score);
+    this.calculator();
+  }
+
+  public async calcMethod(calcMethod: CalcMethod): Promise<void> {
+    this.room.options.calcMethod = calcMethod;
+    await getManager().save(Room, this.room);
+    this.calculator();
+  }
+
+  public async nextStory(): Promise<void> {
+    if (this.currentStory) {
+      this.currentStory.score = Scrum.initResults[this.currentScore];
+      this.currentStory.isCompleted = true;
+      await getManager().save(Story, this.currentStory);
+    }
+
+    const userIds = this.room.userRooms
+      .filter(ur => !ur.isLeft && (!ur.isHost || this.room.options.needScore))
+      .map(ur => ur.userId);
+
+    await this.startNextStory(userIds);
+  }
+
+  public async changeCurrentScore(currentScore: number): Promise<void> {
+    this.room.options.calcMethod = CalcMethod.Customized;
+    this.currentScore = currentScore;
+    await getManager().save(Room, this.room);
+  }
+
+  public async addStories(stories: string[], user: User): Promise<void> {
+    await getManager().transaction(async (transactionalEntityManager) => {
+      for (let i = 0; i < stories.length; i += 1) {
+        const story = new Story();
+        story.name = stories[i];
+        story.roomId = this.room.id;
+        story.creatorId = user.id;
+        story.updaterId = user.id;
+        await transactionalEntityManager.insert(Story, story);
+        story.scores = [];
+        this.room.stories.push(story);
+      }
+    });
+
+    if (!this.currentStory) {
+      await this.nextStory();
+    }
+  }
+
+  private async handleTimer(user: User, userRoom: UserRoom): Promise<void> {
+    if (this.room.userRooms.every(r => r.isLeft)) {
+      if (this.currentStory) {
+        await getManager().save(Story, this.currentStory);
+      }
+
+      if (this.timer) {
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+
+      this.currentStory = null;
+      this.currentScore = null;
+      if (this.onDestory) {
+        this.onDestory(this);
+      }
+    } else {
+      const needScore = this.room.options.needScore || !userRoom.isHost;
+      if (this.currentStory) {
+        if (needScore) {
+          await this.createScore(user.id);
+        }
+      } else {
+        await this.startNextStory(needScore ? [user.id] : []);
+      }
+    }
+  }
+
+  private async startNextStory(userIds: number[]): Promise<void> {
+    this.currentScore = null;
+    this.currentStory = this.room.stories.find(s => !s.isDeleted && !s.isCompleted);
+    if (this.currentStory) {
+      if (!this.timer) {
+        this.timer = setInterval(
+          () => this.currentStory.timer += 1,
+          1000,
+        );
+      }
+
+      for (let i = 0; i < userIds.length; i += 1) {
+        await this.createScore(userIds[i]);
+      }
+
+      this.calculator();
+    } else {
+      if (this.timer) {
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+    }
+  }
+
+  private async createUserRoom(user: User, isLeft: boolean): Promise<UserRoom> {
+    let userRoom = this.room.userRooms.find(ur => ur.userId === user.id);
+    const exist = !!userRoom;
+    if (!exist) {
+      userRoom = new UserRoom();
+      userRoom.userId = user.id;
+      userRoom.roomId = this.room.id;
+      userRoom.isHost = this.room.creatorId === user.id;
+    }
+
+    userRoom.isLeft = isLeft;
+    await getManager().save(UserRoom, userRoom);
+    if (!exist) {
+      this.room.userRooms.push(userRoom);
+    }
+
+    return userRoom;
+  }
+
+  private async createScore(userId: number): Promise<Score> {
+    let score = this.currentStory.scores.find(s => s.userId === userId);
+    if (!score) {
+      score = new Score();
+      score.userId = userId;
+      score.storyId = this.currentStory.id;
+      await getManager().save(Score, score);
+      this.currentStory.scores.push(score);
+    }
+
+    return score;
+  }
+
+  private calculator(): void {
+    const { calcMethod } = this.room.options;
+    if (calcMethod === 3) {
+      return;
+    }
+
+    const scores = this.currentStory.scores
+      .map(s => s.card)
+      .filter(s => s !== null && s >= 0)
+      .sort((a, b) => a - b);
+
+    if (scores.length === 0) {
+      this.currentScore = null;
+      return;
+    }
+
+    if (scores.length > 2 && calcMethod === 1) {
+      scores.pop();
+      scores.splice(0, 1);
+    }
+
+    const { length } = scores;
+    let result: number;
+    if (calcMethod === 0) {
+      result = scores.reduce((v, s) => v + s, 0) / length;
+    } else {
+      result = length % 2 === 0 ?
+        Math.round((scores[length / 2] + scores[length / 2 - 1]) / 2) : scores[(length - 1) / 2];
+    }
+
+    this.currentScore = Scrum.initResults.map((value, index) => ({
+      value,
+      index,
+      abs: Math.abs(value - result),
+    })).sort((i, j) => {
+      if (i.abs > j.abs) {
+        return 1;
+      }
+
+      if (i.abs < j.abs) {
+        return -1;
+      }
+
+      return j.value - i.value;
+    })[0].index;
+  }
+
 }
---
+++ server/src/util/__tests__/Scrum.spec.ts
@@ -0,0 +1,261 @@
+import { createConnection, getManager, getConnectionOptions } from 'typeorm';
+import { config } from '../../config';
+import { Room, Score, Story, User, UserRoom } from '../../entity';
+import { CalcMethod } from '../../model';
+import { Scrum } from '../Scrum';
+
+describe('Scrum', () => {
+  let host: User;
+  let players: User[];
+  let room: Room;
+  let scrum: Scrum;
+  const onDestory = jest.fn();
+
+  beforeAll(async () => {
+    const connectionOptions = await getConnectionOptions();
+    Object.assign(connectionOptions, {
+      synchronize: true,
+      logging: false,
+      bigNumberStrings: false,
+      entities: [
+        Room,
+        Score,
+        Story,
+        User,
+        UserRoom,
+      ],
+    });
+    if (connectionOptions.type === 'mysql') {
+      Object.assign(connectionOptions, { bigNumberStrings: true });
+    }
+
+    await createConnection(connectionOptions);
+
+    await getManager().transaction(async (transactionalEntityManager) => {
+      for (let i = 3; i >= 0; i -= 1) {
+        const user = new User();
+        user.nickName = i === 0 ? '[Jest] Test Host' : `[Jest] Test Player ${3 - i}`;
+        await transactionalEntityManager.insert(User, user);
+      }
+
+      room = new Room();
+      room.name = '[Jest] Test Room';
+      room.options = {
+        needScore: true,
+        isNoymous: false,
+        calcMethod: CalcMethod.ArithmeticMean,
+      };
+      room.creatorId = host.id;
+      room.updaterId = host.id;
+      await transactionalEntityManager.insert(Room, room);
+
+      for (let i = 0; i < 2; i += 1) {
+        const story = new Story();
+        story.name = `[Jest] Test Story ${i + 1}`;
+        story.roomId = room.id;
+        story.creatorId = host.id;
+        story.updaterId = host.id;
+        await transactionalEntityManager.insert(Story, story);
+      }
+    });
+
+    [host, ...players] = await getManager().find(User, { take: 4, order: { id: 'DESC' } });
+
+    room = await Scrum.getRoom(room.id);
+    scrum = new Scrum(room, onDestory);
+  });
+
+  it('host join room', async () => {
+    await scrum.join(host);
+    const userRoom = scrum.room.userRooms.find(us => us.userId === host.id);
+    expect(userRoom).not.toBeNull();
+    expect(userRoom.isLeft).toBeFalsy();
+  });
+
+  it('host leave room', async () => {
+    await scrum.leave(host);
+    expect(onDestory).toBeCalled();
+    const userRoom = scrum.room.userRooms.find(us => us.userId === host.id);
+    expect(userRoom).not.toBeNull();
+    expect(userRoom.isLeft).toBeTruthy();
+  });
+
+  it('on destory', async () => {
+    await scrum.join(host);
+    await scrum.leave(host);
+    const times = onDestory.mock.calls.length;
+
+    await scrum.join(host);
+    await scrum.join(players[0]);
+    await scrum.leave(host);
+    expect(onDestory).toBeCalledTimes(times);
+    await scrum.leave(players[0]);
+    expect(onDestory).toBeCalledTimes(times + 1);
+  });
+
+  it('select card', async () => {
+    await scrum.join(host);
+    const score = scrum.currentStory.scores.find(s => s.userId === host.id);
+    expect(score).not.toBeNull();
+
+    await scrum.selectCard(host, 1);
+    expect(score.card).toBe(1);
+    expect(scrum.currentScore).toBe(2);
+
+    await scrum.selectCard(host, 2);
+    expect(score.card).toBe(2);
+    expect(scrum.currentScore).toBe(3);
+
+    await scrum.leave(host);
+  });
+
+  it('change current score', async () => {
+    await scrum.join(host);
+    await scrum.changeCurrentScore(1);
+    expect(scrum.currentScore).toBe(1);
+    expect(scrum.room.options.calcMethod).toBe(CalcMethod.Customized);
+    await scrum.leave(host);
+  });
+
+  it('change calc method', async () => {
+    await scrum.join(host);
+    await scrum.join(players[0]);
+    await scrum.join(players[1]);
+    await scrum.join(players[2]);
+
+    await scrum.selectCard(host, 1);
+    await scrum.selectCard(players[0], 2);
+    await scrum.selectCard(players[1], 10);
+
+    await scrum.calcMethod(CalcMethod.ArithmeticMean);
+    expect(scrum.room.options.calcMethod).toBe(CalcMethod.ArithmeticMean);
+    expect(scrum.currentScore).toBe(5); // [1, 2, 10] ArithmeticMean -> 4
+
+    await scrum.calcMethod(CalcMethod.TruncatedMean);
+    expect(scrum.room.options.calcMethod).toBe(CalcMethod.TruncatedMean);
+    expect(scrum.currentScore).toBe(3); // [1, 2, 10] TruncatedMean -> 2
+
+    await scrum.calcMethod(CalcMethod.Median);
+    expect(scrum.room.options.calcMethod).toBe(CalcMethod.Median);
+    expect(scrum.currentScore).toBe(3); // [1, 2, 10] Median -> 2
+    await scrum.selectCard(players[2], 4);
+    expect(scrum.currentScore).toBe(4); // [1, 2, 4, 10] Median -> 3
+
+    await scrum.calcMethod(CalcMethod.Customized);
+    expect(scrum.room.options.calcMethod).toBe(CalcMethod.Customized);
+    expect(scrum.currentScore).toBe(4); // [1, 2, 4, 10] Customized -> 3
+
+    await scrum.leave(host);
+    await scrum.leave(players[0]);
+    await scrum.leave(players[1]);
+    await scrum.leave(players[2]);
+  });
+
+  it('time changes', async () => {
+    await scrum.join(host);
+    const { timer } = scrum.currentStory;
+    await new Promise(resolve => setTimeout(resolve, 1050));
+    expect(scrum.currentStory.timer).toBe(timer + 1);
+    await scrum.leave(host);
+  });
+
+  it('next story', async () => {
+    await scrum.join(host);
+    while (scrum.currentStory) {
+      const { currentStory } = scrum;
+      await scrum.nextStory();
+      expect(currentStory.isCompleted).toBeTruthy();
+    }
+    expect(scrum.currentStory).toBeUndefined();
+    await scrum.selectCard(host, 1);
+    await scrum.nextStory();
+    await scrum.leave(host);
+  });
+
+  it('add stories', async () => {
+    await scrum.join(host);
+    while (scrum.currentStory) {
+      await scrum.nextStory();
+    }
+    await scrum.addStories(['[Jest] Test Add Story 1'], host);
+    expect(scrum.currentStory).toBeDefined();
+    expect(scrum.currentStory.name).toBe('[Jest] Test Add Story 1');
+
+    await scrum.addStories(['[Jest] Test Add Story 2'], host);
+    expect(scrum.currentStory.name).toBe('[Jest] Test Add Story 1');
+
+    await scrum.leave(host);
+  });
+
+  it('host not score', async () => {
+    let room2: Room;
+
+    await getManager().transaction(async (transactionalEntityManager) => {
+      room2 = new Room();
+      room2.name = '[Jest] Test Room 2';
+      room2.options = {
+        needScore: false,
+        isNoymous: false,
+        calcMethod: CalcMethod.ArithmeticMean,
+      };
+      room2.creator = host;
+      room2.updater = host;
+      await transactionalEntityManager.insert(Room, room2);
+
+      for (let i = 0; i < 2; i += 1) {
+        const story = new Story();
+        story.name = `[Jest] Test Story ${i + 1}`;
+        story.room = room2;
+        story.creator = host;
+        story.updater = host;
+        await transactionalEntityManager.insert(Story, story);
+      }
+    });
+
+    room2 = await getManager().findOneOrFail(Room, {
+      relations: [
+        'userRooms',
+        'userRooms.user',
+        'stories',
+        'stories.scores',
+        'stories.scores.user',
+        'creator',
+        'updater',
+      ],
+      where: {
+        id: room2.id,
+      },
+    });
+
+    const scrum2 = new Scrum(room2);
+    await scrum2.join(players[0]);
+    await scrum2.join(host);
+
+    await scrum2.leave(players[0]);
+    await scrum2.leave(host);
+
+    await scrum2.join(host);
+    await scrum2.join(players[0]);
+
+    while (scrum2.currentStory) {
+      const { currentStory } = scrum2;
+      await scrum2.nextStory();
+      expect(currentStory.isCompleted).toBeTruthy();
+    }
+    expect(scrum2.currentStory).toBeUndefined();
+
+    await scrum2.leave(players[0]);
+    await scrum2.leave(host);
+
+    room2.isDeleted = true;
+    await getManager().save(Room, room2);
+  });
+
+  afterAll(async () => {
+    if (room) {
+      room.isDeleted = true;
+      await getManager().save(Room, room);
+    }
+  });
+
+});
